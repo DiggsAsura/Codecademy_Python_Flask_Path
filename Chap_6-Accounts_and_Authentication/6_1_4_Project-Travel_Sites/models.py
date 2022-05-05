61d0ff80e269d63a5c8c1bcd95293842a7b6e127
DiggsAsura <kenneth.bjerke@gmail.com>
2022-05-05 09:16:17 +0200

    Chap_6-Accounts_and_Authentication/

--- Chap_6-Accounts_and_Authentication/6_1_4_Project-Travel_Sites/models.py
+++ Chap_6-Accounts_and_Authentication/6_1_4_Project-Travel_Sites/models.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from app import db, login
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(64), index=True, unique=True)
   email = db.Column(db.String(120), index=True, unique=True)
@@ -19,8 +20,6 @@ class User(db.Model):
   def check_password_hash(self, password):
     return check_password_hash(self.password_hash, password)
 
-
-
 class Post():
     id = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String(140))
@@ -31,4 +30,8 @@ class Post():
 
     def __repr__(self):
         return '<Post {}>'.format(self.description)
+
+@login.user_loader
+def load_user(id):
+  return User.query.get(int(id))
 
